f07f03cd7f2b400c7845e25ff298b13b0bdca341
JochemBeurskens <beurskensjochem@hotmail.com>
2021-06-30 00:04:23 +0200

    added a lot of measurements

--- wiener.m
+++ wiener.m
@@ -1,4 +1,4 @@
-function [h0, h1, h2] = wiener(a, b)
+function [h0, h1, h2] = wiener(a, b,trunc_start, scaling_factor, mean_compensation)
 %
 %      [h0, h1, h2] = wiener(a, b);
 %
@@ -24,12 +24,15 @@ end
 if nargin<1
    a= 1;
 end
-  
+if nargin<5
+   mean_compensation= 0; %this factor is added for the case where the predictions seem to be off by a constant, which is the case for a large non-linear part
+else
+   mean_compensation= 1; 
+end
+
 gwnt = 0.5 * randn(1,4000);    % gaussian noise with power (close to) 0.5
 %now check the gwnt signal for its properties
 close all;
-%gwnt = 0.5 * randn(1,4000);
-%gwnt2 = 0.5 * randn(1,4000);
 [f,xi] = ksdensity(gwnt,'Function','pdf'); 
 sigma=std(gwnt);
 mu=mean(gwnt);
@@ -40,30 +43,18 @@ figure(100)
 plot(xi,f);
 hold on
 plot(x,g);
+title('Amplitude distribution')
 legend('Measured','Theoretical')
 xlabel('x')
 ylabel('P(x)')
 
 fs = 1000; % sample frequency (Hz), is 1000 Hz as dt is 1ms, as resolution of signals is 1ms
-%now using a sinusoidal example from mathworks.com, to compare the values
-%to. Note that the power range of this signal is way larger, thus we can
-%conclude that our signal is nearly flat.
-%uncommend the code below to show the sinusoidal. And uncommend the hold on
-%and the second pspectrum in figure 101.
-% Fs = 1000;
-% t = (0:1/Fs:0.296)';
-% x = cos(2*pi*t*200)+0.1*randn(size(t));
-% xTable = timetable(seconds(t),x);
 
-%[power,freqs]=pspectrum(gwnt,fs);
 figure(101)
-%plot(freqs,power);
 pspectrum(gwnt,fs);
-% hold on
-% pspectrum(xTable);
-% legend('GWN','Sinusoidal with noise, example')
 xlabel('Frequency (Hz)')
 ylabel('Power (dB)')
+title('Power spectrum')
 %seems flat, especially when compared to the spectrum of eg. a sinusoid
 
 deviation=3999;
@@ -71,9 +62,11 @@ a_corr_gwn_xx=phi_yx2(gwnt,gwnt,deviation);
 figure(102)
 xlim([0 deviation])
 plot(a_corr_gwn_xx)
+title('Full autocorrelation')
 figure(103)
 xlim([0 200])
 plot(a_corr_gwn_xx(1:200))
+title('First 200 time lags of the autocorrelation')
 ylabel('\phi_{xx}')
 xlabel('\tau')
 %end of property check
@@ -97,9 +90,9 @@ ut = ut(1:length(gwnt)); % only the first gwnt samples
 
 figure(4)
 plot(time, ut, 'r-');
+title('The linear and non-linear parts of the signal and the second order prediction')
 hold on
 yt = a*ut + b*ut.^2;    % the static non-linearity
-
 plot(time, yt, 'k-');
 
 
@@ -108,13 +101,16 @@ plot(time, yt, 'k-');
 %
 h0=mean(yt);
 G0 = h0;       % the zero-order Wiener prediction
-y0 = yt - G0;      % the Lee-Schetzen method: subtract h0 from the response
-
+if mean_compensation==0
+    y0 = yt - G0;      % the Lee-Schetzen method: subtract h0 from the response
+else 
+    y0 = yt;
+end
 %  first order estimate: crosscorrelate
 %
 fi1 = phi_yx2(y0, gwnt, length(tf)-1);   %first-order kernel
 h1=fi1/P;       % only positive tau! 
-h1(150:end)=0; %listened to the hint, this reduced the noise a lot indeed
+h1(trunc_start:end)=0; %listened to the hint, this reduced the noise a lot indeed
 %although I think a closer approximation is obtained for 200:end being 0,
 %as there is some information between 150 and 200 (in the shape of a peak)
 s=sum((h1)); %get a sum over h1 of one if we take away the absolute value, but get worse results
@@ -146,8 +142,11 @@ title('Predicted first-order kernel (h1) and the model linear filter (g)');
 
 G1 = conv(gwnt, h1);      % 1e order Wiener prediction with the scaled kernel 
 G1 = G1(1:length(gwnt));
-
-y1 = yt - G0 - G1;    % residue, corrected for G0 and G1
+if mean_compensation==0
+    y1 = yt - G0 - G1;    % residue, corrected for G0 and G1
+else 
+    y1 = yt - G1;    % residue, corrected for G0 and G1
+end
 
 %
 %    second order kernel: 3rd-order cross-correlation
@@ -166,15 +165,31 @@ disp(sum(sum(h2))*dt);
 G2 = convh2xx(h2,gwnt,P,1);      % 1e order Wiener prediction with the scaled kernel 
 figure(4)
 plot(G0+G1+G2); %/8.5) %when ht is not normalised then we need to divide h1 by ~8.5 for them to have the same order
-legend('u(t)','y(t)','Prediction');
+legend('u(t)','y(t)','Total prediction (always includes G0)');
+
+figure(29)
+plot(G1);
+title('The linear part of the signal, y0, and the prediction from the first order kernel, G1')
+hold on
+plot(y0);
+legend('G1','y0')
 
 figure(10)
 plot(G2);
+title('The non-linear part of the signal, y1, and the prediction from the second order kernel, G2')
 hold on
+plot(y1);
+legend('G2','y1')
+figure(11)
 plot(yt);
+title('The signal, and the second order prediction')
 hold on
-plot(G0+G1+G2);
-legend('G2','y(t)','total')
+if mean_compensation==0
+    plot((G0+G1+G2)/scaling_factor);
+else 
+    plot((G1+G2)/scaling_factor);
+end
+legend('y(t)','second order')
 disp("corr second order and yt");
 disp(corrcoef((G0+G1+G2),yt));
 disp("corr first order and yt");
@@ -188,6 +203,19 @@ imagesc(h2(1:100,1:100));    % Look parallel to the main diagonal!
 axis xy
 colorbar;
 title('Second-order kernel')
+
+disp("Mean and standard deviation of the difference between the signal and the second order estimation");
+disp(mean(abs(yt-((G0+G1+G2)/scaling_factor))));
+disp(std(abs(yt-((G0+G1+G2)/scaling_factor))));
+disp("Mean and standard deviation of the difference between the signal and the first order estimation");
+disp(mean(abs(yt-((G0+G1)/scaling_factor))));
+disp(std(abs(yt-((G0+G1)/scaling_factor))));
+disp("Mean and standard deviation of the difference between the signal and G1+G2");
+disp(mean(abs(yt-((G1+G2)/scaling_factor))));
+disp(std(abs(yt-((G1+G2)/scaling_factor))));
+disp("Mean and standard deviation of the difference between the signal and G1");
+disp(mean(abs(yt-((G1)/scaling_factor))));
+disp(std(abs(yt-((G1)/scaling_factor))));
 
 figure(5)
 Diagh2 = zeros(1,length(tf));
